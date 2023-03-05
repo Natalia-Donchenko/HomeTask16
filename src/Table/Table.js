6cf23a66529cc695d0eb780729332f476f834ca3
Natalia.Donchenko <n.donchenko1991@gmail.com>
2023-03-05 10:33:31 +0200

    1.0

---
+++ src/Table/Table.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default props => (
+  <table className='table'>
+    <thead>
+      <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Email</th>
+        <th>Gender</th>
+        <th>Age</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      { props.data.users.map(item => (
+        <tr key={item.id}>
+          <td>{item.firstName}</td>
+          <td>{item.lastName}</td>
+          <td>{item.email}</td>
+          <td>{item.gender}</td>
+          <td>{item.age}</td>
+          <td className='btn btn-light'>Edit</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+)
