3e000d225c717c1b2e194103108738f5a18f9fd5
Natalia.Donchenko <n.donchenko1991@gmail.com>
2023-03-06 22:39:57 +0200

    1.2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,159 @@
 import React, { Component } from 'react';
 import { data } from './users-data';
-import Table from './Table/Table'
-import Buttons from './Buttons/Buttons'
-import Form from './Form/Form'
-import './Form/form.css'
+import './styles.css';
+import { useState } from 'react';
 
+function App() {
+  const userData = deepClone(data.users);
+  const[users, setUsers] = useState(userData);
+  const[editingUser, setEditingUser] = useState({});
+  const[isShowForm, setisShowForm] = useState(false);
 
-class App extends Component {
-
-   
-  render() {
-    return (
-      <div className='container'>
-        <Form />
-        <Table data={data}/>
-      </div>
-      
-    );
+  const handleUserEdit = (value) => {
+    const user = users.find((el) => el.id === +value);
+    setEditingUser(user);
   }
+
+  const handleUserSubmit = () => {
+    const editingUserIndex = users.findIndex(user => user.id === editingUser.id);
+    const updatedUsers = [...users];
+
+    updatedUsers[editingUserIndex]=editingUser;
+    setUsers(updatedUsers);
+
+    setisShowForm(false);
+ }
+   
+  const handleFirstNameChange = (event) => {
+    setEditingUser({...editingUser, firstName: event.target.value});
+  }
+
+  const handleLastNameChange = (event) => {
+    setEditingUser({...editingUser, lastName: event.target.value});
+  }
+
+  const handleEmailChange = (event) => {
+    setEditingUser({...editingUser, email: event.target.value});
+  }
+
+  const handleAgeChange = (event) => {
+    setEditingUser({...editingUser, age: event.target.value});
+  }
+
+  return (
+    <div className='container'>
+      {isShowForm && (
+        <div className='wrapper'>
+          <div className='form'>
+            <h1 className='form__title'>User Editor</h1>
+            <form className='form__editor' editingUser={editingUser} onSubmit={handleUserEdit}>
+        
+              <label className='form__editor-label'>Firts Name*</label>
+              <input 
+                className='form__editor_input' 
+                value={editingUser.firstName} 
+                onChange={handleFirstNameChange}
+                id="firstName" 
+                type="text" 
+                name="first_name">
+              </input>
+
+              <label className='form__editor-label'>Last Name*</label>
+              <input 
+                className='form__editor_input' 
+                value={editingUser.lastName} 
+                onChange={handleLastNameChange}
+                id="lastName" 
+                type="text" 
+                name="last_name"> 
+              </input>
+
+              <label className='form__editor-label'>Email</label>
+              <input 
+                className='form__editor_input' 
+                value={editingUser.email} 
+                onChange={handleEmailChange}
+                id="email" 
+                type="email" 
+                name="email">
+              </input>
+
+              <label className='form__editor-label'>Age</label>
+              <input
+                className='form__editor_input'
+                value={editingUser.age}
+                onChange={handleAgeChange}
+                id="age"
+                type="number"
+                name="age">
+              </input>
+
+            </form>
+
+            <button 
+              className='btn btn-primary' 
+              type="submit"
+              onClick={handleUserSubmit}>
+              Submit
+            </button>
+          </div>
+        </div>
+      )}
+      
+      <table 
+        className='table' 
+        data={data}>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Gender</th>
+            <th>Age</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          { users.map(user => (
+            <tr key={user.id}>
+              <td>{user.firstName}</td>
+              <td>{user.lastName}</td>
+              <td>{user.email}</td>
+              <td>{user.gender}</td>
+              <td>{user.age}</td>
+              <td>
+                <button 
+                  id={user.id} 
+                  className='btn btn-light' 
+                  onClick={() => {
+                    setEditingUser(user)
+                    setisShowForm(true)
+                  }}>
+                  Edit
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>   
+    </div>
+  )
 }
 
-export default App;
+const deepClone = (originalObj) => {
+  if (!originalObj || typeof originalObj !== "object")  { 
+    return originalObj; 
+  }
 
-// console.log(data.users)
+  const clonedObj = Array.isArray(originalObj) ? [] : {};
+
+  for (const key in originalObj) {
+    const value = originalObj[key];
+
+    clonedObj[key] = deepClone(value);
+  }
+
+  return clonedObj;
+};
+
+export default App;
